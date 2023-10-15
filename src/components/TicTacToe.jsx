3da43c98947ce74842955e82123c9ccc0590f682
Chantity <chantityb@gmail.com>
2023-10-15 14:12:36 -0400

    added onClick functionality

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -5,17 +5,28 @@ const Player_X = "X";
 const Player_O = "O";
 
 function TicTacToe() {
-    const [tiles, setTiles] = useState(Array(9).fill(null));
-    const [playerTurn, setPlayerTurn] = useState(Player_X);
+    const [tiles, setTiles] = useState(Array(9).fill(null)); // array of 9 nulls
+    const [playerTurn, setPlayerTurn] = useState(Player_X); // X goes first
     
     const handleTileClick = (index) => {
-        console.log(index);
+        
+        if (tiles[index] !== null) return; // if tile is already filled, return (do nothing else)
+        
+        //console.log(index);
+        const newTiles = [...tiles]; // copy of tiles array
+        newTiles[index] = playerTurn; // update the value of the clicked tile
+        setTiles(newTiles); // update the tiles array with the new value of the clicked tile
+        if (playerTurn === Player_X) {
+            setPlayerTurn(Player_O);
+        } else {
+            setPlayerTurn(Player_X);
+        } // update the playerTurn state variable to the next player
     }
 
     return ( 
         <div>
             <h1>Tic Tac Toe</h1>
-            <Board tiles={tiles} onTileClick={handleTileClick} />
+            <Board playerTurn={playerTurn} tiles={tiles} onTileClick={handleTileClick} />
         </div>
      );
 }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,6 +1,12 @@
-function Tile({className, value}) {
+function Tile({className, value, onClick, playerTurn}) {
+    
+    let hoverClass = null;
+    if (value === null && playerTurn != null) {
+        hoverClass = `${playerTurn.toLowerCase()}-hover`
+    }
+    
     return ( 
-        <div className={`tile ${className}`}>{value}</div>
+        <div onClick={onClick} className={`tile ${className} ${hoverClass}`}>{value}</div> // onClick is a prop passed from Board.jsx to Tile.jsx to handleTileClick in TicTacToe.jsx 
      );
 }
 
